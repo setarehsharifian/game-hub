db1049a2226daced6ade114dfcbe96a7cdc4e0e6
setarehsharifian <setarehsharifian33@gmail.com>
2026-07-16 21:17:15 +0330

    implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 const App = () => {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -20,7 +21,9 @@ const App = () => {
       templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
       <GridItem area="nav">
-        <Navbar />
+        <Navbar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -16,6 +16,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        serach: gameQuery.searchText,
       },
     },
     [gameQuery],
